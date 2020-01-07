98ef5035472edefc45277e3c065ea476c92582a7
Benjamin Bulis <bbulis@student.tgm.ac.at>
2020-01-07 16:22:17 +0100

    Added Data to Database

--- backend/app.js
+++ backend/app.js
@@ -7,16 +7,49 @@ const Schueler = require('./models/schueler')
 
 const PORT = process.env.PORT || 3000
 
-app.get('/schueler', (req, res) => {
-    res.send(JSON.stringify({ "type": "GET-Request" }, null, 2))
+app.get('/schueler', async(req, res) => {
+    let schuelers = await Schueler.findAll()
+    res.send(JSON.stringify({ "success": "true", "data": schuelers }))
+})
+
+app.delete('/schueler/:id', async(req, res) => {
+    try {
+        let result = await Schueler.destroy({ where: { id: req.params.id } })
+        if (result == 1) {
+            res.send(JSON.stringify({ "deleted": "true" }))
+        } else {
+            res.send(JSON.stringify({ "deleted": "false" }))
+        }
+    } catch (err) {
+        res.send(JSON.stringify({ "error": "true", "data": err }))
+    }
+})
+
+app.get('/reset', async(req, res) => {
+    await Schueler.sync({ force: true })
+    let schueler1 = await Schueler.create({
+        firstname: 'Benjamin',
+        lastname: 'Bulis',
+        klasse: '5CHIT',
+        zweig: 'MEDT'
+    })
+
+    let schueler2 = await Schueler.create({
+        firstname: 'David',
+        lastname: 'Langheiter',
+        klasse: '5CHIT',
+        zweig: 'SYT'
+    })
+
+    res.send(JSON.stringify({ "success": "true", "data": [schueler1, schueler2] }))
 })
 
 app.get('/testing', async(req, res) => {
     try {
         result = await sequelize.authenticate()
-        res.send(JSON.stringify({ "type": "success" }, null, 3))
+        res.send(JSON.stringify({ "type": "success" }))
     } catch (err) {
-        res.send(JSON.stringify(err, null, 3))
+        res.send(JSON.stringify(err))
     }
 })
 
