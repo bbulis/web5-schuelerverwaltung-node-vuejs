b58e182b44c4889541fe07bfdfaa6a98ed0d4517
Benjamin Bulis <bbulis@student.tgm.ac.at>
2020-01-08 08:35:12 +0100

    Added more Endpoints and edited DockerCompose File

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,51 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 const Sequelize = require('sequelize')
 const sequelize = require('./config')
 const Schueler = require('./models/schueler')
 
 const PORT = process.env.PORT || 3000
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/schueler', async(req, res) => {
     let schuelers = await Schueler.findAll()
-    res.send(JSON.stringify({ "success": "true", "data": schuelers }))
+    res.send(JSON.stringify({ "success": true, "data": schuelers }))
+})
+
+app.get('/schueler/:id', async(req, res) => {
+    let schueler = await Schueler.findByPk(req.params.id)
+    if (schueler) {
+        res.send(JSON.stringify({ "success": true, "data": schueler }))
+    } else {
+        res.send(JSON.stringify({ "success": false, "data": "no schueler found" }))
+    }
+})
+
+app.post('/schueler', async(req, res) => {
+    let Inputfirstname = req.body.firstname
+    let Inputlastname = req.body.lastname
+    let Inputklasse = req.body.klasse
+    let Inputzweig = req.body.zweig
+
+    let [schueler, created] = await Schueler.findOrCreate({ where: { firstname: Inputfirstname, lastname: Inputlastname, klasse: Inputklasse, zweig: Inputzweig } })
+
 })
 
 app.delete('/schueler/:id', async(req, res) => {
     try {
-        let result = await Schueler.destroy({ where: { id: req.params.id } })
+        let result = await Schueler.destroy({
+            where: { id: req.params.id }
+        })
         if (result == 1) {
-            res.send(JSON.stringify({ "deleted": "true" }))
+            res.send(JSON.stringify({ "deleted": true }))
         } else {
-            res.send(JSON.stringify({ "deleted": "false" }))
+            res.send(JSON.stringify({ "deleted": false }))
         }
     } catch (err) {
-        res.send(JSON.stringify({ "error": "true", "data": err }))
+        res.send(JSON.stringify({ "error": true, "data": err }))
     }
 })
 
@@ -41,7 +65,7 @@ app.get('/reset', async(req, res) => {
         zweig: 'SYT'
     })
 
-    res.send(JSON.stringify({ "success": "true", "data": [schueler1, schueler2] }))
+    res.send(JSON.stringify({ "success": true, "data": [schueler1, schueler2] }))
 })
 
 app.get('/testing', async(req, res) => {
